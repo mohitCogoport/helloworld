da1b9737f70c8cc029139d72ed652f11a0b0c614
mohitCogoport <mohit.raj@cogoport.com>
2022-06-03 12:32:13 +0530

    dd

--- src/Dashboard/Charts/Trend/icons/index.js
+++ src/Dashboard/Charts/Trend/icons/index.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import * as React from "react";
 
-const IcDownSvg = () => (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8 16C3.58172 16 -1.56562e-07 12.4183 -3.49691e-07 8C-5.4282e-07 3.58172 3.58172 -1.56562e-07 8 -3.49691e-07C12.4183 -5.4282e-07 16 3.58172 16 8C16 12.4183 12.4183 16 8 16Z" fill="#CB6464"/>
-<path d="M8.00003 11.2002L8.00003 4.8002M8.00003 11.2002L5.8667 8.9602M8.00003 11.2002L10.1334 8.9602" stroke="white" stroke-linecap="round"/>
-</svg>
-)
+const IcDownSvg = (props) => (
+  <svg
+    width={16}
+    height={16}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M8 16A8 8 0 1 1 8 0a8 8 0 0 1 0 16Z" fill="#CB6464" />
+    <path
+      d="M8 11.2V4.8m0 6.4L5.867 8.96M8 11.2l2.133-2.24"
+      stroke="#fff"
+      strokeLinecap="round"
+    />
+  </svg>
+);
 
-const IcUpSvg = () => (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M8 -3.49691e-07C12.4183 -1.56562e-07 16 3.58172 16 8C16 12.4183 12.4183 16 8 16C3.58172 16 4.10854e-07 12.4183 6.03983e-07 8C7.97112e-07 3.58172 3.58172 -5.4282e-07 8 -3.49691e-07Z" fill="#67C676"/>
-<path d="M7.99997 4.7998L7.99997 11.1998M7.99997 4.7998L10.1333 7.0398M7.99997 4.7998L5.86663 7.0398" stroke="white" stroke-linecap="round"/>
-</svg>
-)
+const IcUpSvg = (props) => (
+  <svg
+    width={16}
+    height={16}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0Z" fill="#67C676" />
+    <path
+      d="M8 4.8v6.4m0-6.4 2.133 2.24M8 4.8 5.867 7.04"
+      stroke="#fff"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
 
 export  {IcDownSvg, IcUpSvg}
