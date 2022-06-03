d12838d6a61ea885bbbdff9ad5f7c5399fde2264
mohitCogoport <mohit.raj@cogoport.com>
2022-06-03 12:03:23 +0530

    d

--- rollup.config.js
+++ rollup.config.js
@@ -6,8 +6,9 @@ import svg from 'rollup-plugin-svg'
 import json from '@rollup/plugin-json';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import {terser} from 'rollup-plugin-terser'
+// import {terser} from 'rollup-plugin-terser'
 // import svgr from '@svgr/rollup'
+import React from 'react';
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 const outputFile = NODE_ENV === "production" ? "./lib/prod.js" : "./lib/dev.js";
@@ -16,7 +17,11 @@ const x={
     input: "./src/Dashboard/DashboardPage/index.js",
     output: {
       file: outputFile,
-      format: "cjs" 
+      format: "cjs",
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      }
     },
     plugins: [
       replace({
@@ -24,7 +29,9 @@ const x={
       }),
       babel({
         exclude: "node_modules/**",
-        presets: ["@babel/preset-react"],
+        presets: [["@babel/preset-react", {
+      "runtime": "automatic"
+      }]],
         babelHelpers: "bundled",
       }),
       resolve(),
@@ -33,9 +40,9 @@ const x={
       json(),
       postcss(),
       peerDepsExternal(),
-      terser()
+      // terser()
     ],
-    external: id => /^react|styled-jsx/.test(id)
+    external: [ 'react' ,'styled-jsx', 'react-dom' ],
 }
 
 export default x;
