180463ea5ce0032c4680272c6def1ca66ad2d9b2
mohitCogoport <mohit.raj@cogoport.com>
2022-06-03 20:11:40 +0530

    s

--- src/Dashboard/Charts/Trend/index.js
+++ src/Dashboard/Charts/Trend/index.js
@@ -1,7 +1,7 @@
 import { Flex, Text } from '@cogoport/front/components';
-// import TrendBlackSvg from '../../assets/charts/trend-black.svg';
-// import IcUpSvg from './ic-up.svg';
-// import IcDownSvg from './ic-down.svg';
+import TrendBlackSvg from '../../assets/charts/trend-black.svg';
+import IcUpSvg from './ic-up.svg';
+import IcDownSvg from './ic-down.svg';
 
 const Trend = ({ data, themeType = 'purple_orange' }) => {
 	const newValue = +data?.chartData?.[1]?.value || 0;
@@ -17,7 +17,7 @@ const Trend = ({ data, themeType = 'purple_orange' }) => {
 
 	return (
 		<Flex direction="column" alignItems="center" width="100%" style={{ gap: '8px' }}>
-			{/* <TrendBlackSvg width={24} height={24} /> */}
+			<TrendBlackSvg width={24} height={24} />
 
 			<Flex direction="column" alignItems="center">
 				<Text size={24} bold>{newValue}</Text>
@@ -25,11 +25,11 @@ const Trend = ({ data, themeType = 'purple_orange' }) => {
 			</Flex>
 
 			<Flex alignItems="center" style={{ gap: '4px' }}>
-				{/* {
+				{
 					isPercentageDownwards
 						? <IcDownSvg width={20} height={20} />
 						: <IcUpSvg width={20} height={20} />
-				} */}
+				}
 
 				<Text as="span" size={18} style={{ color: 'rgb(116, 131, 143)' }}>•</Text>
 
