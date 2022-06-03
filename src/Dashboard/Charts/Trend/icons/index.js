ebddfd7c5c5e263cfdd55f0f66a805306f89838c
mohitCogoport <mohit.raj@cogoport.com>
2022-06-03 12:16:49 +0530

    DD

---
+++ src/Dashboard/Charts/Trend/icons/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const IcDownSvg = () => (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8 16C3.58172 16 -1.56562e-07 12.4183 -3.49691e-07 8C-5.4282e-07 3.58172 3.58172 -1.56562e-07 8 -3.49691e-07C12.4183 -5.4282e-07 16 3.58172 16 8C16 12.4183 12.4183 16 8 16Z" fill="#CB6464"/>
+<path d="M8.00003 11.2002L8.00003 4.8002M8.00003 11.2002L5.8667 8.9602M8.00003 11.2002L10.1334 8.9602" stroke="white" stroke-linecap="round"/>
+</svg>
+)
+
+const IcUpSvg = () => (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8 -3.49691e-07C12.4183 -1.56562e-07 16 3.58172 16 8C16 12.4183 12.4183 16 8 16C3.58172 16 4.10854e-07 12.4183 6.03983e-07 8C7.97112e-07 3.58172 3.58172 -5.4282e-07 8 -3.49691e-07Z" fill="#67C676"/>
+<path d="M7.99997 4.7998L7.99997 11.1998M7.99997 4.7998L10.1333 7.0398M7.99997 4.7998L5.86663 7.0398" stroke="white" stroke-linecap="round"/>
+</svg>
+)
+
+export  {IcDownSvg, IcUpSvg}
